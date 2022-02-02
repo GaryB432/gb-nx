1323193538ca85bd611ebb51e6f3e828d00562a0
Gary Bortosky <garybort@gmail.com>
2022-02-02 15:47:07 -0600

    add extension generator

---
+++ packages/nx-browser/src/generators/extension/generator.ts
@@ -0,0 +1,80 @@
+import {
+  addProjectConfiguration,
+  formatFiles,
+  generateFiles,
+  getWorkspaceLayout,
+  names,
+  offsetFromRoot,
+  Tree,
+} from '@nrwl/devkit';
+import * as path from 'path';
+import initGenerator from '../init/generator';
+import { ExtensionGeneratorSchema } from './schema';
+
+interface NormalizedSchema extends ExtensionGeneratorSchema {
+  projectName: string;
+  projectRoot: string;
+  projectDirectory: string;
+  parsedTags: string[];
+}
+
+function normalizeOptions(
+  tree: Tree,
+  options: ExtensionGeneratorSchema
+): NormalizedSchema {
+  const name = names(options.name).fileName;
+  const projectDirectory = options.directory
+    ? `${names(options.directory).fileName}/${name}`
+    : name;
+  const projectName = projectDirectory.replace(new RegExp('/', 'g'), '-');
+  const projectRoot = `${getWorkspaceLayout(tree).appsDir}/${projectDirectory}`;
+  const parsedTags = options.tags
+    ? options.tags.split(',').map((s) => s.trim())
+    : [];
+
+  return {
+    ...options,
+    projectName,
+    projectRoot,
+    projectDirectory,
+    parsedTags,
+  };
+}
+
+function addFiles(tree: Tree, options: NormalizedSchema) {
+  const templateOptions = {
+    ...options,
+    ...names(options.name),
+    offsetFromRoot: offsetFromRoot(options.projectRoot),
+    tmpl: '',
+  };
+  generateFiles(
+    tree,
+    path.join(__dirname, 'files'),
+    options.projectRoot,
+    templateOptions
+  );
+}
+
+export default async function (tree: Tree, options: ExtensionGeneratorSchema) {
+  const normalizedOptions = normalizeOptions(tree, options);
+  addProjectConfiguration(tree, normalizedOptions.projectName, {
+    root: normalizedOptions.projectRoot,
+    projectType: 'application',
+    sourceRoot: `${normalizedOptions.projectRoot}/src`,
+    targets: {
+      build: {
+        executor: '@gb-nx/nx-browser:build',
+        outputs: ['{options.outputPath}'],
+        options: {
+          outputPath: `dist/${normalizedOptions.projectRoot}/extension`,
+          manifest: `${normalizedOptions.projectRoot}/src/manifest`,
+        },
+      },
+    },
+    tags: normalizedOptions.parsedTags,
+  });
+  addFiles(tree, normalizedOptions);
+  (await initGenerator(tree, { ...options, skipFormat: true }))();
+  await formatFiles(tree);
+}
