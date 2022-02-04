b3f257e111c19de43aa93391575fded0837395f6
Gary Bortosky <garybort@gmail.com>
2022-02-04 14:37:35 -0600

    add custom linting

--- packages/nx-browser/src/generators/extension/generator.ts
+++ packages/nx-browser/src/generators/extension/generator.ts
@@ -2,20 +2,117 @@ import {
   addProjectConfiguration,
   formatFiles,
   generateFiles,
+  GeneratorCallback,
   getWorkspaceLayout,
+  joinPathFragments,
   names,
   offsetFromRoot,
+  readJson,
   Tree,
+  writeJson,
 } from '@nrwl/devkit';
-import * as path from 'path';
+import { jestProjectGenerator } from '@nrwl/jest';
+import { Linter, lintProjectGenerator } from '@nrwl/linter';
+import { join } from 'path';
 import initGenerator from '../init/generator';
 import { ExtensionGeneratorSchema } from './schema';
+
+interface LintOverride {
+  files: string[];
+  extends?: string[];
+}
 
 interface NormalizedSchema extends ExtensionGeneratorSchema {
   parsedTags: string[];
   projectDirectory: string;
   projectName: string;
   projectRoot: string;
+  // setParserOptionsProject: boolean;
+  // testEnvironment: any;
+  // compiler: any;
+}
+
+function addGbLintPlugin(tree: Tree, options: NormalizedSchema): void {
+  // console.log(options);
+  const { root, plugins } = readJson(tree, '.eslintrc.json') as {
+    root: boolean;
+    plugins: string[];
+  };
+  // console.log({ root, plugins });
+}
+
+function addCustomLint(tree: Tree, options: NormalizedSchema): void {
+  const projectRc = joinPathFragments(options.projectRoot, '.eslintrc.json');
+  const customPath = 'eslint-custom.json';
+  const rc = readJson(tree, projectRc) as {
+    overrides: LintOverride[];
+  };
+  if (!rc.overrides) {
+    rc.overrides = [{ files: ['*.ts'] }];
+  }
+  let tso = rc.overrides.find(
+    (o) => o.files.includes('*.ts') && !o.files.includes('*.js')
+  );
+  if (!tso) {
+    tso = { files: ['*.ts', '*.tsx'] };
+    rc.overrides.push(tso);
+  }
+  tso.extends = tso.extends ?? [];
+  tso.extends.push(
+    joinPathFragments(offsetFromRoot(options.projectRoot), customPath)
+  );
+  writeJson(tree, projectRc, rc);
+  writeJson(tree, customPath, {
+    overrides: [
+      {
+        files: ['*.ts'],
+        rules: {
+          '@typescript-eslint/no-unused-vars': 'off',
+          '@typescript-eslint/member-ordering': 'warn',
+        },
+      },
+      {
+        files: ['*spec.ts'],
+        rules: {
+          '@typescript-eslint/no-non-null-assertion': 'warn',
+        },
+      },
+    ],
+  });
+}
+
+async function addJest(
+  tree: Tree,
+  options: NormalizedSchema
+): Promise<GeneratorCallback> {
+  return await jestProjectGenerator(tree, {
+    project: options.name,
+    setupFile: 'none',
+    supportTsx: false,
+    skipSerializers: true,
+    testEnvironment: 'jsdom',
+    skipFormat: true,
+    // compiler: options.compiler,
+  });
+}
+
+function addLint(
+  tree: Tree,
+  options: NormalizedSchema
+): Promise<GeneratorCallback> {
+  addGbLintPlugin(tree, options);
+  const generateLint = lintProjectGenerator(tree, {
+    project: options.name,
+    linter: Linter.EsLint,
+    skipFormat: true,
+    tsConfigPaths: [
+      joinPathFragments(options.projectRoot, 'tsconfig.lib.json'),
+    ],
+    eslintFilePatterns: [`${options.projectRoot}/**/*.ts`],
+    setParserOptionsProject: true,
+  });
+  addCustomLint(tree, options);
+  return generateLint;
 }
 
 function normalizeOptions(
@@ -32,12 +129,17 @@ function normalizeOptions(
     ? options.tags.split(',').map((s) => s.trim())
     : [];
 
+  // options.unitTestRunner = 'none';
+  // options.linter = Linter.None;
+
   return {
     ...options,
     projectName,
     projectRoot,
     projectDirectory,
     parsedTags,
+    // setParserOptionsProject: true,
+    // testEnvironment: 'jsdom',
   };
 }
 
@@ -50,7 +152,7 @@ function addFiles(tree: Tree, options: NormalizedSchema) {
   };
   generateFiles(
     tree,
-    path.join(__dirname, 'files'),
+    join(__dirname, 'files'),
     options.projectRoot,
     templateOptions
   );
@@ -77,7 +179,9 @@ export default async function (
     },
     tags: normalizedOptions.parsedTags,
   });
+  await initGenerator(tree, { ...normalizedOptions, skipFormat: true });
   addFiles(tree, normalizedOptions);
-  (await initGenerator(tree, { ...options, skipFormat: true }))();
+  await addJest(tree, normalizedOptions);
+  await addLint(tree, normalizedOptions);
   await formatFiles(tree);
 }
--- packages/nx-browser/src/generators/extension/schema.d.ts
+++ packages/nx-browser/src/generators/extension/schema.d.ts
@@ -1,5 +1,9 @@
 export interface ExtensionGeneratorSchema {
   directory?: string;
   name: string;
+  setParserOptionsProject?: boolean;
+  skipFormat?: boolean;
+  skipTests?: boolean;
   tags?: string;
+  unitTestRunner?: 'jest' | 'none';
 }
