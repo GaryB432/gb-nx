4ce507e9d7bc83311a68e52d1acff4455606b58e
Gary Bortosky <garybort@gmail.com>
2022-08-12 11:35:43 -0500

    spruce up make-schemas

--- tools/scripts/make-schemas.ts
+++ tools/scripts/make-schemas.ts
@@ -1,7 +1,8 @@
+import { output } from '@nrwl/devkit';
 import { readFile, writeFile } from 'fs/promises';
 import { compileFromFile } from 'json-schema-to-typescript';
-import { join, parse, ParsedPath, posix } from 'path';
-import { Workspaces } from '@nrwl/tao/src/shared/workspace';
+import { Workspaces } from 'nx/src/config/workspaces';
+import { join, parse, ParsedPath } from 'path';
 
 interface SchemaDefined {
   schema: string;
@@ -21,6 +22,21 @@ const argv = process.argv.slice(2);
 const ws = new Workspaces(process.cwd());
 const wsConfig = ws.readWorkspaceConfiguration();
 
+const messages: string[] = [];
+
+function getDefFileName(path: ParsedPath, color = false): string {
+  if (!color) {
+    return [path.name, ...argv, 'd', 'ts'].join('.');
+  }
+  const plusArgs = ['', ...argv];
+  const parts = [
+    output.colors.cyan(join(path.dir, path.name)),
+    output.colors.red(plusArgs.map((v) => `${v}`).join('.')),
+    output.colors.cyan('.d.ts'),
+  ];
+  return parts.join('');
+}
+
 async function writeSchemaTypeDef(
   root: string,
   sdef: SchemaDefined,
@@ -31,10 +47,8 @@ async function writeSchemaTypeDef(
     /* from ${sdef.schema} */`,
     strictIndexSignatures: true,
   });
-  void writeFile(
-    join(root, path.dir, [path.name, ...argv, 'd', 'ts'].join('.')),
-    dts
-  );
+  messages.push(getDefFileName(path, true));
+  void writeFile(join(root, path.dir, getDefFileName(path)), dts);
 }
 
 async function handleSchemaDefList(
@@ -46,19 +60,23 @@ async function handleSchemaDefList(
   const buf = await readFile(join(root, defListPath));
   const sds = JSON.parse(buf.toString()) as SchemaDefinedList;
   for (const sdef of Object.values(sds[k])) {
-    console.log(root, sdef.schema);
     await writeSchemaTypeDef(root, sdef, parse(sdef.schema));
   }
 }
 
 async function main() {
   for (const projName in wsConfig.projects) {
+    void messages.splice(0, messages.length);
     const proj = wsConfig.projects[projName];
     const pjb = await readFile(join(proj.root, 'package.json'));
     const pj = JSON.parse(pjb.toString()) as PackageConfig;
     const { root } = proj;
     await handleSchemaDefList(root, pj.executors, 'executors');
     await handleSchemaDefList(root, pj.generators, 'generators');
+    output.log({
+      title: `Schema definitions generated for project '${projName}'`,
+      bodyLines: messages,
+    });
   }
 }
 
