c39146081d9fc596112c45f1cc29cfa75fff46b0
Gary Bortosky <garybort@gmail.com>
2022-10-14 16:12:09 -0500

    update for jest config

--- packages/junit/src/generators/junit/generator.spec.ts
+++ packages/junit/src/generators/junit/generator.spec.ts
@@ -1,46 +1,148 @@
-import type { Tree } from '@nrwl/devkit';
-import { readProjectConfiguration } from '@nrwl/devkit';
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import type { TargetConfiguration, Tree } from '@nrwl/devkit';
+import {
+  addProjectConfiguration,
+  readProjectConfiguration,
+} from '@nrwl/devkit';
 import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
-
+// import { projectGraphAdapter } from 'nx/src/project-graph/project-graph';
 import generator from './generator';
-import type { Schema as NxJunitGeneratorSchema } from './schema';
+import type { Schema as JunitGeneratorSchema } from './schema';
+
+const jestConfigScript = `/* eslint-disable */
+export default {
+  displayName: 'test',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/test',
+};`;
+
+const jestConfigScriptWithReporters = `/* eslint-disable */
+export default {
+  displayName: 'test',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/test',
+  reporters: [
+    'other',
+    [
+      'jest-party',
+      {
+        confetti: true,
+        moar: 'clowns.xml',
+      },
+    ],
+  ],
+};`;
+
+// const goal: JestConfig = {
+//   preset: '../../jest.preset.js',
+//   testEnvironment: 'node',
+//   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+//   coverageDirectory: '../../coverage/apps/test',
+//   reporters: [
+//     'default',
+//     [
+//       'jest-junit',
+//       {
+//         outputDirectory: 'junit/libs',
+//         outputName: 'test.xml',
+//       },
+//     ],
+//     'other',
+//     [
+//       'jest-tbd',
+//       {
+//         outputDirectory: 'party/libs',
+//         outputName: 'alphabetical',
+//       },
+//     ],
+//   ],
+// };
 
 describe('junit generator', () => {
   let appTree: Tree;
-  const options: NxJunitGeneratorSchema = { projectName: 'test' };
-
-  const po = {
-    targets: {
-      test: {
-        executor: '@nrwl/jest:jest',
-        outputs: ['coverage/apps/test'],
-        options: { jestConfig: 'apps/test/jest.config.js' },
-      },
-    },
+  const options: JunitGeneratorSchema = {
+    project: 'test',
   };
-
-  const fddf = { version: 2, projects: { test: 'apps/test' } };
-
-  const jc = (module.exports = {
-    displayName: 'test',
-    junk: 'sure',
-  });
 
   beforeEach(() => {
     appTree = createTreeWithEmptyWorkspace();
-    // console.log(po.targets.test.options.jestConfig);
-    appTree.write('workspace.json', JSON.stringify(fddf));
-    appTree.write('apps/test/project.json', JSON.stringify(po));
-    // appTree.write(
-    //   po.targets.test.options.jestConfig,
-    //   "module.exports = { displayName: 'test', junk: 'yes' };"
-    // );
-    // console.log(appTree.listChanges());
+    appTree.write(
+      'package.json',
+      JSON.stringify({
+        name: 'fun@test',
+        dependencies: { cheese: 'latest' },
+        devDependencies: {},
+      })
+    );
+    addProjectConfiguration(appTree, 'test', {
+      root: 'apps/test',
+      name: 'test',
+      sourceRoot: 'apps/test/src',
+      projectType: 'application',
+      targets: {
+        fun: {
+          executor: '@gb-nx/hello',
+          outputs: ['{options.outputFile}'],
+          options: {
+            sandwiches: ['classic', 'jelly'],
+          },
+        },
+        test: {
+          executor: '@nrwl/jest:jest',
+          outputs: [
+            'coverage/apps/test',
+            'more/files/test',
+            'way/more/than/normal',
+          ],
+          options: {
+            jestConfig: 'apps/test/jest.config.ts',
+          },
+        },
+      },
+      tags: [],
+    });
+    appTree.write('apps/test/jest.config.ts', jestConfigScript);
   });
 
-  it.skip('should run successfully', async () => {
+  it('should run successfully', async () => {
     await generator(appTree, options);
     const config = readProjectConfiguration(appTree, 'test');
     expect(config).toBeDefined();
+  });
+
+  it('should create files', async () => {
+    await generator(appTree, options);
+    const config = readProjectConfiguration(appTree, 'test');
+    const target: TargetConfiguration = config.targets!['test'];
+    expect(target.options!.jestConfig).toEqual('apps/test/jest.config.ts');
+    const jconf = appTree.read(target.options!.jestConfig);
+    expect(jconf!.toString()).toContain("outputDirectory: 'junit/apps'");
+  });
+
+  it('should add output', async () => {
+    await generator(appTree, options);
+    const config = readProjectConfiguration(appTree, 'test');
+    const target: TargetConfiguration = config.targets!['test'];
+    expect(target.options!.jestConfig).toEqual('apps/test/jest.config.ts');
+    expect(target.outputs).toEqual([
+      'coverage/apps/test',
+      'more/files/test',
+      'way/more/than/normal',
+    ]);
   });
 });
--- packages/junit/src/generators/junit/generator.ts
+++ packages/junit/src/generators/junit/generator.ts
@@ -1,14 +1,14 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import type { ProjectType, Tree } from '@nrwl/devkit';
+import type { GeneratorCallback, Tree } from '@nrwl/devkit';
 import {
   addDependenciesToPackageJson,
   formatFiles,
   getProjects,
   getWorkspaceLayout,
-  installPackagesTask,
+  joinPathFragments,
+  logger,
   names,
 } from '@nrwl/devkit';
-import { posix } from 'path';
 import * as ts from 'typescript';
 import type { Schema as NxJunitGeneratorSchema } from './schema';
 
@@ -17,80 +17,69 @@ interface NormalizedSchema extends NxJunitGeneratorSchema {
 }
 
 function handleJestConfig(sourceFile: ts.SourceFile, rptrs: string): string {
-  const exp = sourceFile.getChildAt(0).getChildAt(0) as ts.ExpressionStatement;
-
   const sourceText = sourceFile.getFullText();
-  const bex = exp.getChildAt(0) as ts.BinaryExpression;
-  if (bex.kind === ts.SyntaxKind.BinaryExpression) {
-    if (bex.left.getText() === 'module.exports') {
-      const ole = bex.right as ts.ObjectLiteralExpression;
-      const lt = ole.getLastToken()!;
-      const position = lt.getFullStart();
-      let alreadyReporters = false;
-      ole.forEachChild((n: ts.Node) => {
-        if (n.kind === ts.SyntaxKind.PropertyAssignment) {
-          const pa = n as ts.PropertyAssignment;
-          if (pa.name.getText() === 'reporters') {
-            alreadyReporters = true;
-          }
-        }
-      });
+  const ea = sourceFile.getChildAt(0).getChildAt(0) as ts.ExportAssignment;
 
-      return alreadyReporters
-        ? sourceText
-        : [
-            sourceText.slice(0, position),
-            rptrs,
-            sourceText.slice(position),
-          ].join('');
+  if (ea.kind !== ts.SyntaxKind.ExportAssignment) {
+    logger.warn('unexpected configuration');
+  }
+
+  const ole = ea.expression as ts.ObjectLiteralExpression;
+  const lt = ole.getLastToken()!;
+  const position = lt.getFullStart();
+  let alreadyReporters = false;
+  for (const p of ole.properties) {
+    if (p.name && p.name.getText() === 'reporters') {
+      alreadyReporters = true;
     }
   }
-  return sourceText;
+  return alreadyReporters
+    ? sourceText
+    : [sourceText.slice(0, position), rptrs, sourceText.slice(position)].join(
+        ''
+      );
 }
 
 function normalizeOptions(
   _tree: Tree,
   options: NxJunitGeneratorSchema
 ): NormalizedSchema {
-  const projectName = options.projectName || 'N/A';
-  const reporterVersion = options.reporterVersion ?? '^13.0.0';
-  return { projectName, reporterVersion, tbd: true };
+  const project = options.project || 'N/A';
+  const reporterVersion = options.reporterVersion ?? '^14.0.0';
+  return { project, reporterVersion, tbd: true };
 }
 
 export default async function (
   tree: Tree,
   options: NxJunitGeneratorSchema
-): Promise<void> {
+): Promise<GeneratorCallback> {
   const normalizedOptions = normalizeOptions(tree, options);
   const ws = getWorkspaceLayout(tree);
   const ps = getProjects(tree);
 
-  const proj = ps.get(normalizedOptions.projectName!);
+  const proj = ps.get(normalizedOptions.project ?? '');
 
   if (!proj) {
     throw new Error('project not found');
   }
 
   const testtarget = proj.targets!['test'];
-
-  const dirMap = new Map<ProjectType, string>([
-    ['application', ws.appsDir],
-    ['library', ws.libsDir],
-  ]);
-
-  const outputDirectory = posix.join('junit', dirMap.get(proj.projectType!)!);
-  const outputName = names(normalizedOptions.projectName!).fileName + '.xml';
+  const outputDirectory = joinPathFragments(
+    'junit',
+    proj.projectType === 'application' ? ws.appsDir : ws.libsDir
+  );
+  const outputName = names(normalizedOptions.project!).fileName + '.xml';
 
   if (testtarget) {
     const buff = tree.read(testtarget.options.jestConfig);
-    const contents = buff ? buff.toString() : 'module.exports = {};';
+    const contents = buff ? buff.toString() : 'export default {};';
     const newContents = handleJestConfig(
       ts.createSourceFile(
         '',
         contents,
         ts.ScriptTarget.ES2015,
         true,
-        ts.ScriptKind.JS
+        ts.ScriptKind.TS
       ),
       `reporters: [
         'default',
@@ -107,7 +96,7 @@ export default async function (
       tree.write(testtarget.options.jestConfig, newContents);
     }
   }
-  addDependenciesToPackageJson(
+  const installTask = addDependenciesToPackageJson(
     tree,
     {},
     {
@@ -116,5 +105,8 @@ export default async function (
   );
 
   await formatFiles(tree);
-  return installPackagesTask(tree);
+
+  return async () => {
+    await installTask();
+  };
 }
