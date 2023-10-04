224d035772434a5bc23965bdf2bf0ead2a855ec8
Gary Bortosky <garybort@gmail.com>
2023-10-04 13:57:26 -0500

    stub junit

---
+++ packages/workspace/src/generators/junit/schema.d.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+
+/**
+ * Add Junit reporter to your project
+ */
+export interface Schema {
+  /**
+   * The project to target.
+   */
+  project?: string;
+  /**
+   * Version of jest-junit to use
+   */
+  reporterVersion?: string;
+}
