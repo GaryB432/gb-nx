45070ffc9cf18eb63638eb9224538e7c7aac41ce
Gary Bortosky <garybort@gmail.com>
2023-12-23 16:08:08 -0600

    bump versions

--- packages/svelte/src/utils/versions.ts
+++ packages/svelte/src/utils/versions.ts
@@ -1,5 +1,5 @@
-export const eslintPluginGbVersion = '^1.5.0';
-export const eslintPluginSvelteVersion = '^2.14.1';
-export const eslintVersion = '^8.28.0';
-export const prettierPluginSvelteVersion = '^2.7.0';
-export const typescriptEslintVersion = '^5.46.1';
+export const eslintPluginGbVersion = '^2.0.0';
+export const eslintPluginSvelteVersion = '^2.0.0';
+export const eslintVersion = '^8.0.0';
+export const prettierPluginSvelteVersion = '^3.0.0';
+export const typescriptEslintVersion = '^6.0.0';
